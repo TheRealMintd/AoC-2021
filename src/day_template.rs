bd5cfbcaabdd5e8ff2df9d2ad0e0c2a06d082480
Wong Yi Xiong <johnmax2468@gmail.com>
2021-12-01 15:10:06 +0800

    Add a file template for each day

---
+++ src/day_template.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<usize> {}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[usize]) -> usize {}
+
+// #[aoc(day1, part2)]
+// pub fn solve_part2(input: &[usize]) -> usize {
+// }
