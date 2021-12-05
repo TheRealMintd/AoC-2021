af752bf955dd98c30d1a5b75682c8cf5ebbfa384
Wong Yi Xiong <johnmax2468@gmail.com>
2021-12-05 14:54:42 +0800

    Complete Day 5

---
+++ src/day_5.rs
@@ -0,0 +1,120 @@
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+};
+
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+
+pub struct VentLine {
+    end_one: Point,
+    end_two: Point,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Point {
+    x: u32,
+    y: u32,
+}
+
+impl Point {
+    fn new(x: u32, y: u32) -> Self {
+        Point { x, y }
+    }
+}
+
+impl From<(u32, u32)> for Point {
+    fn from((x, y): (u32, u32)) -> Self {
+        Self::new(x, y)
+    }
+}
+
+#[aoc_generator(day5)]
+pub fn input_generator(input: &str) -> Vec<VentLine> {
+    input
+        .lines()
+        .map(|line| {
+            let points: ((u32, u32), (u32, u32)) = line
+                .split(" -> ")
+                .map(|coordinates| {
+                    coordinates
+                        .split(',')
+                        .map(|position| position.parse().unwrap())
+                        .collect_tuple()
+                        .unwrap()
+                })
+                .collect_tuple()
+                .unwrap();
+
+            VentLine {
+                end_one: points.0.into(),
+                end_two: points.1.into(),
+            }
+        })
+        .collect()
+}
+
+#[aoc(day5, part1)]
+pub fn solve_part1(input: &[VentLine]) -> usize {
+    input
+        .into_iter()
+        .filter(|vent| vent.end_one.x == vent.end_two.x || vent.end_one.y == vent.end_two.y)
+        .flat_map::<Vec<Point>, _>(|vent| {
+            if vent.end_one.x == vent.end_two.x {
+                (min(vent.end_one.y, vent.end_two.y)..=max(vent.end_one.y, vent.end_two.y))
+                    .map(|y| (vent.end_one.x, y).into())
+                    .collect()
+            } else {
+                (min(vent.end_one.x, vent.end_two.x)..=max(vent.end_one.x, vent.end_two.x))
+                    .map(|x| (x, vent.end_one.y).into())
+                    .collect()
+            }
+        })
+        .fold(HashMap::new(), |mut map: HashMap<_, u32>, point| {
+            *map.entry(point).or_insert(0) += 1;
+            map
+        })
+        .into_iter()
+        .filter(|(_, count)| *count > 1)
+        .count()
+}
+
+#[aoc(day5, part2)]
+pub fn solve_part2(input: &[VentLine]) -> usize {
+    input
+        .into_iter()
+        .flat_map::<Vec<Point>, _>(|vent| {
+            if vent.end_one.x == vent.end_two.x {
+                (min(vent.end_one.y, vent.end_two.y)..=max(vent.end_one.y, vent.end_two.y))
+                    .map(|y| (vent.end_one.x, y).into())
+                    .collect()
+            } else if vent.end_one.y == vent.end_two.y {
+                (min(vent.end_one.x, vent.end_two.x)..=max(vent.end_one.x, vent.end_two.x))
+                    .map(|x| (x, vent.end_one.y).into())
+                    .collect()
+            } else {
+                let x_range: Box<dyn Iterator<Item = u32>> = if vent.end_one.x > vent.end_two.x {
+                    Box::new((vent.end_two.x..=vent.end_one.x).rev())
+                } else {
+                    Box::new(vent.end_one.x..=vent.end_two.x)
+                };
+                let y_range: Box<dyn Iterator<Item = u32>> = if vent.end_one.y > vent.end_two.y {
+                    Box::new((vent.end_two.y..=vent.end_one.y).rev())
+                } else {
+                    Box::new(vent.end_one.y..=vent.end_two.y)
+                };
+
+                x_range
+                    .zip(y_range)
+                    .map(|coordinates| coordinates.into())
+                    .collect()
+            }
+        })
+        .fold(HashMap::new(), |mut map: HashMap<_, u32>, point| {
+            *map.entry(point).or_insert(0) += 1;
+            map
+        })
+        .into_iter()
+        .filter(|(_, count)| *count > 1)
+        .count()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ pub mod day_1;
 pub mod day_2;
 pub mod day_3;
 pub mod day_4;
+pub mod day_5;
 
 aoc_lib! { year = 2021 }
